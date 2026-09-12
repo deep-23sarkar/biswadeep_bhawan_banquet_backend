8740e3354b188e6ff1154ae189a38ae4ea05ab34
Deep Sarkar <161959401+deep-23sarkar@users.noreply.github.com>
2026-09-13 04:36:04 +0530

    Cors changes

--- src/main/java/com/biswadeep/banquetHall/config/CorsConfig.java
+++ src/main/java/com/biswadeep/banquetHall/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","http://localhost:3001","https://biswadeep-bhawan-banquet.vercel.app/","https://biswadeep-bhawan-banquet-guga.vercel.app/")
+                        .allowedOrigins("http://localhost:3000","http://localhost:3001","https://biswadeep-bhawan-banquet.vercel.app","https://biswadeep-bhawan-banquet-guga.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
